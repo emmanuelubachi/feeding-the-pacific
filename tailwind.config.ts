4d05750db3818383e472d1a735810a1ece422943
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-10-02 05:18:42 +0400

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "pri-1": "#919e8b",
+        "pri-12": "#e3e0cd",
+        "pri-13": "#cfaa23",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
